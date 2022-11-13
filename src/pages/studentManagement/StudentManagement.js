8e8e3a24724a297bb120c884b67fdba343a9ed29
KritapasSuwannawin <kritapas.ms@gmail.com>
2022-11-13 18:53:28 +0700

    bugfix studentManagement

--- src/pages/studentManagement/StudentManagement.js
+++ src/pages/studentManagement/StudentManagement.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
